9d5099b8616be92d0aaaf233ce904e527ebe21a7
Asim Poudel <asimdkt63@gmail.com>
2026-02-08 14:13:27 +0545

    add form.js

---
+++ js/form.js
@@ -0,0 +1,32 @@
+function createForm() {
+  var $form = $("<form></form>");
+
+  $form.html(`
+        <h2>grocery list helper</h2>
+        <div class ='form-control'>
+        <input
+        type = "text"
+        class= "form-input"
+        placeholder = "e.g. eggs"
+        />
+        <button type ="submit" class = "btn">
+        add item
+        </button>
+        </div>
+        `);
+
+  $form.on("submit", function (e) {
+    e.preventDefault();
+    var $input = $form.find(".form-input");
+    var value = $.trim($input.val());
+    if (!value) {
+      alert("Please provide value");
+      return;
+    }
+
+    addItem(value);
+    $input.val("");
+  });
+
+  return $form;
+}
